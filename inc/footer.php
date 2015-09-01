95ec9e8108772168ccbb2931e5dfb5ec1a032844
etp1711 <etp1711@gmail.com>
2015-09-01 18:42:22 +0430

    asd

--- inc/footer.php
+++ inc/footer.php
@@ -3,11 +3,12 @@
 	<div class="contain">
 		<div class="box1">
 	<!--	<h4>شبکه های اجتماعی</h4>  --><h4><a href="contact.php">تمـــــاس بــا مــا</a></h4>
-			<p><a href="contact.php"></a></p>	 
-	<br/><img src="images/face.png" />
-			<img src="images/instagram.png" style="margin-right:5px;" />
-			<img src="images/flickr.png" style="margin-right:5px;" />
-			<img src="images/youtube.png" style="width:60px; height:30px; margin-right:5px;" />
+			<p><a href="contact.php"></a></p><br/>
+
+			<a href="https://www.facebook.com/SouleMast?ref=hl">	<img src="images/face.png" /></a>
+			<a href="https://instagram.com/soulemast">				<img src="images/instagram.png" style="margin-right:5px;" /></a>
+			<a href="contact.php">									<img src="images/gmail.png" style="margin-right:5px;" /></a>
+			<a href="">												<img src="images/youtube.png" style="width:60px; height:30px; margin-right:5px;" /></a>
 		</div>
 		<div class="box2">
 			<h4><a href="learning.php">آمــوزش تـنــــــــبور</a></h4>
